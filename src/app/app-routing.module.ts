b9b711f64736af5b09d098f1690476cbf7b5d36e
MDz1985 <m_d@tut.by>
2022-08-01 23:23:02 +0200

    refactor: make basic project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'form',
+    loadChildren: () => import('./form/form.module')
+      .then(result => result.FormModule)
+  },
+  {
+    path: '',
+    redirectTo: 'form',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
